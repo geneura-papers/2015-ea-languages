e557a3af9e5998879d44222b3ad8946c77870759
M42 <mromang08@gmail.com>
2015-12-13 22:11:00 +0100

    Completes Bitflip algorithm Using random vectors

--- haskell/bitflip.hs
+++ haskell/bitflip.hs
@@ -4,9 +4,12 @@ import Data.Vector.Unboxed.Mutable as V
 import Data.Vector.Unboxed as U
 import Control.Monad.Primitive
 import Control.Monad
+import Control.Monad.Random
+import System.Random
+import Data.List
 
 iterations = 100000
-len = 16
+len = 5
 
 -- Flips the given bit in a mutable vector, returns an action in m.
 bitflip :: (PrimMonad m) => MVector (PrimState m) Bool -> Int -> m ()
@@ -16,16 +19,32 @@ bitflip v i = do
 
 -- Sequences the reading of every element in a mutable vector.
 toPrintList :: (PrimMonad m, Unbox a) => MVector (PrimState m) a -> m [a]
-toPrintList v = sequence [V.read v n | n <- [1..V.length v-1]]
+toPrintList v = sequence [V.read v n | n <- [0..V.length v-1]]
 
+-- Creates a random boolean list, given a generator
+randomBoolList :: (Random a) => Int -> StdGen -> [a]
+randomBoolList n = Data.List.take n . Data.List.unfoldr (Just . random)
 
-main :: IO [Bool]
+-- Creates a random boolean list, given a generator and a range
+randomIntList :: Int -> (Int,Int) -> StdGen -> [Int]
+randomIntList n (a,b) = Data.List.take n . Data.List.unfoldr (Just . (randomR (a,b)))
+
+main :: IO ()
 main = do
   -- Random vector
   vector <- V.replicate len False
-
-  -- Random bitflips
-  bitflip vector 1
+  let seed = mkStdGen 1
+  let rs = (randomBoolList len seed) :: [Bool]
+  sequence $ Prelude.map (\n -> write vector n (rs!!n)) [0..len-1]
 
   -- Printing
-  Main.toPrintList vector
+  Main.toPrintList vector >>= print
+
+  -- Random bitflips
+  let seed2 = mkStdGen 2
+  let res = (randomIntList iterations (0,len-1) seed2) :: [Int]
+  sequence $ Prelude.map (\pos -> bitflip vector pos) res
+
+  -- Printing
+  Main.toPrintList vector >>= print
+  return ()
