bb189e001ac2073b658fc6ff2838c4085193d54c
M42 <mromang08@gmail.com>
2015-12-31 17:50:01 +0100

    Bitflip using auxiliary functions

--- haskell/bitflip.hs
+++ haskell/bitflip.hs
@@ -9,6 +9,7 @@ import Control.Applicative
 import System.Random
 import Data.List
 import Data.Time
+import Auxiliary
 
 iterations = 100000
 
@@ -18,27 +19,11 @@ bitflip v i = do
   x <- unsafeRead v i
   unsafeWrite v i (not x)
 
--- Sequences the reading of every element in a mutable vector.
-printVector :: (Show a, Unbox a) => MVector (PrimState IO) a -> IO ()
-printVector v = sequence [V.read v n | n <- [0..V.length v-1]] >>= print
-
--- Creates a random boolean list, given a generator
-randomBoolList :: (Random a) => Int -> StdGen -> [a]
-randomBoolList n = Data.List.take n . Data.List.unfoldr (Just . random)
-
--- Creates a random integer list, given a generator and a range
-randomIntList :: Int -> (Int,Int) -> StdGen -> [Int]
-randomIntList n (a,b) = Data.List.take n . Data.List.unfoldr (Just . (randomR (a,b)))
-
 -- Complete benchmark for a given length
 benchmark :: Int -> IO ()
 benchmark len = do
-  -- Random vector
-  vector <- V.replicate len False
-  let seed = mkStdGen 1
+  vector <- randomVector len
   let seed2 = mkStdGen 2
-  let rs = (randomBoolList len seed) :: [Bool]
-  sequence $ Prelude.map (\n -> write vector n (rs!!n)) [0..len-1]
 
   -- Timing
   start <- getCurrentTime
