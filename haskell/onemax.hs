7c4d18549566bd74213173853a87d1488a1a8fa9
M42 <mromang08@gmail.com>
2015-12-31 17:33:21 +0100

    Adds OneMax benchmark in Haskell

---
+++ haskell/onemax.hs
@@ -0,0 +1,48 @@
+-- Data.Vector.Unboxed allows for a specialised and more efficient representation
+-- of a boolean vector.
+import Data.Vector.Unboxed.Mutable as V
+import Data.Vector.Unboxed as U
+import Control.Monad.Primitive
+import Control.Monad
+import Control.Monad.Random
+import Control.Applicative
+import System.Random
+import Data.List
+import Data.Time
+
+-- Creates a random boolean list, given a generator
+randomBoolList :: (Random a) => Int -> StdGen -> [a]
+randomBoolList n = Data.List.take n . Data.List.unfoldr (Just . random)
+
+-- Complete benchmark for a given length
+benchmark :: Int -> IO ()
+benchmark len = do
+  -- Initialization
+  vector <- V.replicate len False
+
+  -- Timing
+  start <- getCurrentTime
+
+  -- Random vector
+  let seed = mkStdGen 1
+  let rs = (randomBoolList len seed) :: [Bool]
+  sequence $ Prelude.map (\n -> write vector n (rs!!n)) [0..len-1]
+             
+  -- Counting
+  sum <- fmap Data.List.sum (sequence (Data.List.map 
+          (\i -> do
+             x <- unsafeRead vector i
+             if x then (return 1) else (return 0)
+          ) [1..len]))
+   
+  stop <- getCurrentTime
+
+  -- Printing
+  putStrLn $ Data.List.concat ["Haskell-Mutable Vector, "
+                              , show len, ", "
+                              , show $ diffUTCTime stop start]
+
+main :: IO ()
+main = do
+  sequence $ Prelude.map benchmark ((2^) <$> [4..16])
+  return ()
