2d02588f06579ee870e3491a90d167c269311234
M42 <mromang08@gmail.com>
2015-12-31 17:04:19 +0100

    Formats output

--- haskell/bitflip.hs
+++ haskell/bitflip.hs
@@ -5,12 +5,12 @@ import Data.Vector.Unboxed as U
 import Control.Monad.Primitive
 import Control.Monad
 import Control.Monad.Random
+import Control.Applicative
 import System.Random
 import Data.List
 import Data.Time
 
 iterations = 100000
-len = 65536
 
 -- Flips the given bit in a mutable vector, returns an action in m.
 bitflip :: (PrimMonad m) => MVector (PrimState m) Bool -> Int -> m ()
@@ -30,27 +30,30 @@ randomBoolList n = Data.List.take n . Data.List.unfoldr (Just . random)
 randomIntList :: Int -> (Int,Int) -> StdGen -> [Int]
 randomIntList n (a,b) = Data.List.take n . Data.List.unfoldr (Just . (randomR (a,b)))
 
-main :: IO ()
-main = do
+-- Complete benchmark for a given length
+benchmark :: Int -> IO ()
+benchmark len = do
   -- Random vector
   vector <- V.replicate len False
   let seed = mkStdGen 1
+  let seed2 = mkStdGen 2
   let rs = (randomBoolList len seed) :: [Bool]
   sequence $ Prelude.map (\n -> write vector n (rs!!n)) [0..len-1]
 
-  -- Printing
-  printVector vector
-
+  -- Timing
   start <- getCurrentTime
 
   -- Random bitflips
-  let seed2 = mkStdGen 2
   let res = (randomIntList iterations (0,len-1) seed2) :: [Int]
   sequence $ Prelude.map (\pos -> bitflip vector pos) res
 
   stop <- getCurrentTime
 
   -- Printing
-  printVector vector
-  print $ diffUTCTime stop start
+  putStrLn $ Data.List.concat ["Haskell-Mutable Vector, ", show len, ", ", show $ diffUTCTime stop start]
+
+
+main :: IO ()
+main = do
+  sequence $ Prelude.map benchmark ((2^) <$> [4..16])
   return ()
