bb31f8c842851fc2169e65922477528a2fb17451
JJ Merelo <jjmerelo@gmail.com>
2016-03-25 09:37:51 +0100

    Processing files

--- ea-languages.R
+++ ea-languages.R
@@ -1,13 +1,16 @@
 ## ----setup, cache=FALSE,echo=FALSE---------------------------------------
 library("ggplot2")
 library(RColorBrewer)
+library("dplyr")
 
 # Load files
 measures.bf <- read.csv('measures-bitflip.csv')
 measures.xo <- read.csv('measures-xover.csv')
 measures.mo <- read.csv('measures-maxones.csv')
+# measures.bf <- measures.bf %>% group_by(length) %>% mutate(NORM = normalit(time))
 
 ## ----results-bf, cache=FALSE,echo=FALSE----------------------------------
+
 colourCount = length(unique(measures.bf$languagerepresentation))
 getPalette = colorRampPalette(brewer.pal(9, "Set1"))
 ggplot(measures.bf,aes(x=length,y=time,colour=factor(languagerepresentation)))+ geom_line() + geom_point() +  ggtitle("Bitflip")+scale_y_log10()+scale_x_log10()+scale_color_manual(name='Language',values=getPalette(colourCount))+ theme(legend.title = element_text(colour="blue", size=7, face="bold"))+ theme(legend.text = element_text(colour="blue", size=5))+ guides(col = guide_legend(keyheight=0.5,keywidth=0.5))
