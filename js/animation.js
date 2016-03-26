df70ec7caaed54762819e5d4acad234577feef8f
JJ Merelo <jjmerelo@gmail.com>
2016-03-26 13:41:59 +0100

    Representing with names and length

--- js/animation.js
+++ js/animation.js
@@ -1,5 +1,7 @@
 // update data
 function update(the_div, data) {
+
+    var this_width = the_div[0][0].offsetWidth;
     
     // DATA JOIN
     // Join new data with old elements, if any.
@@ -8,33 +10,42 @@ function update(the_div, data) {
     // ENTER
     // Create new elements as needed.
     bars.enter().append("div")
-	.style("width", function(d) { 
-	    //			     console.log("ENTER"+d);
-	    return d/100 + "px"; 
+	.style("width", function( data ) {
+	    return scale(data,this_width);
 	})
-	.text(function(d) { return d; });
+	.text(function(d) { return d[" text"]; });
     
     // ENTER + UPDATE
     // Appending to the enter selection expands the update selection to include
     // entering elements; so, operations on the update selection after appending to
     // the enter selection will apply to both entering and updating nodes.
-    bars.style("width", function(d) { 
-	//		     console.log("ENTER+UPDATE"+d);
-	return d/100 + "px"; 
-    }).text(function(d) { return d; });
+    bars.style("width", function( data ) {
+	    return scale(data,this_width);
+    })
+	.text(function(d) { return d["languagerepresentation"]; });
     
     // EXIT
     // Remove old elements as needed.
     bars.exit().remove();
 }
 
+// data should be at most 1
+function scale( data, pixels ) {
+    console.log( data );
+    return data[" normtime"]*pixels + "px"; 
+};
+    
+
 var animation_div = d3.select("#animation");
 d3.csv("mo-256bits.csv",
        function(error,data){
-	   console.log(data);
+	   var sorted_data = data.sort( function( a,b) {
+	       return a [" normtime"] - b[" normtime"];
+	   });
+	   console.log(sorted_data);
 	   var the_bars = new Array;
-	   for (var i in data ) {
-	       the_bars.push( parseFloat(data[i][" normtime"])*100000 );
+	   for (var i in sorted_data ) {
+	       the_bars.push( sorted_data[i] );
 	       console.log(the_bars);
 	       update(animation_div, the_bars );
 	   }
