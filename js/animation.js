379259fc46196f1b6269f184dfd03054f4b8beaa
JJ Merelo <jjmerelo@gmail.com>
2016-03-27 20:20:39 +0200

    For testing purposes

--- js/animation.js
+++ js/animation.js
@@ -9,25 +9,36 @@ function scale( data, pixels ) {
     return data[" normtime"]*pixels + "px"; 
 };
     
-
-var animation_div = d3.select("#animation");
-var this_width = animation_div[0][0].offsetWidth;
-d3.csv("mo-256bits.csv",
-       function(error,data){
-	   var sorted_data = data.sort( function( a,b) {
-	       return a[" normtime"] - b[" normtime"];
-	   });
-	   var the_bars = new Array;
-	   var bars = animation_div.selectAll("div").data(data);
-
-	   bars.enter().append("div")
-	       .style("background-color",  function( data ) {
-		   return colors[data[' datastructure']];
-	       })
-	       .text(function(d) { return d.languagerepresentation; });
-
-	   bars.transition()
-	       .style("width", function( data ) {
-	       return scale(data,this_width);
+// loads data
+function load_data( div_id, file_name, cb ) {
+    var animation_div = d3.select("#"+div_id);
+    var this_width = animation_div[0][0].offsetWidth;
+    d3.csv(file_name,
+	   function(error,data){
+	       var sorted_data = data.sort( function( a,b) {
+		   return a[" normtime"] - b[" normtime"];
 	       });
-       });
+	       var the_bars = new Array;
+	       var bars = animation_div.selectAll("div").data(data);
+	       
+	       bars.enter().append("div")
+		   .style("background-color",  function( data ) {
+		       return colors[data[' datastructure']];
+		   })
+		   .text(function(d) { return d.languagerepresentation; });
+	       console.log( data );
+	       cb(div_id, bars);
+	   })
+}
+
+// animates
+function animate( bars ) {
+    bars.transition()
+	.delay(function( d ) {
+	    return 5000*d[" normtime"]; // scale to 10 seconds
+	})
+	.style("width", function( data ) {
+	    return scale(data,this_width);
+	});
+}
+
